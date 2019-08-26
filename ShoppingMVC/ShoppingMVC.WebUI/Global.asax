33ab1696467be9624e7cce918d018aac270165a6
Akash Rajguru <a.rajguru@research.ait.ie>
2019-08-26 16:26:48 +0100

    Initial Solution setup

---
+++ ShoppingMVC/ShoppingMVC.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingMVC.WebUI.MvcApplication" Language="C#" %>
---
+++ ShoppingMVC/ShoppingMVC.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingMVC.WebUI.Startup))]
+namespace ShoppingMVC.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
